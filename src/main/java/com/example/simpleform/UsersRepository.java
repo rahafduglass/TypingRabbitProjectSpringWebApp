13fe7f49e2ee804d6f54b5b1bc1ca62e05c576de
rahafduglass <duglassrahaf@gmail.com>
2023-12-19 20:27:52 +0300

    commit 2

--- src/main/java/com/example/simpleform/UsersRepository.java
+++ src/main/java/com/example/simpleform/UsersRepository.java
@@ -8,5 +8,5 @@ import java.util.Optional;
 @Repository
 public interface UsersRepository extends CrudRepository<UsersModel, Integer> {
     Optional<UsersModel> findByLoginAndPassword(String login, String password);
-    Optional<UsersModel> findFirstByLogin(String login); // to ensure that the written username in registration doesn't already exists.
+    Optional<UsersModel> findFirstByLogin(String login); // to ensure that the written username in registration doesn't already exist.
 }
